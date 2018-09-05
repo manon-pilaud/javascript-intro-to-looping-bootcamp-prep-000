2124edb866c2757c26b481ab332236b7f5b879b0
Mandy Pilaud <mandyypilaud@gmail.com>
2018-09-05 21:34:03 +0000

    automatically backed up by learn

---
+++ loops.js
@@ -0,0 +1,33 @@
+function forLoop(arr){
+     for (var i=0 ;i<25;i++){
+      if (i===1){
+          arr.push("I am 1 strange loop.")
+      }
+      else{
+        arr.push(`I am ${i} strange loops.`)
+      }
+    }
+    return arr
+}
+
+function whileLoop(number){
+  let countdown = parseInt(number);
+  while (countdown > 0) {
+    console.log(--countdown);
+    if(countdown === 0) {
+      return 'done'
+  }
+  }
+}
+
+function doWhileLoop(array) {
+var i = 0
+
+function incrementVariable(){
+  i -= 1;
+}
+do {
+  incrementVariable()
+} while (array.length > 0);
+return array
+}
